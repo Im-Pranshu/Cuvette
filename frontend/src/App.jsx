3167c950f757012b6edc662fd6534627a7363d7c
Im-Pranshu <pranshu.sharma1303@gmail.com>
2025-04-11 19:36:19 +0530

    updated urls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "https://your-backend-url.onrender.com";
+const API = "https://student-job-backend-t6er.onrender.com";
 
 function App() {
   const [jobs, setJobs] = useState([]);
